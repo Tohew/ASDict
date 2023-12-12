5cdea60df6a1669f3d3bf73eb33972211d801f45
nghgi <22520357@gm.uit.edu.vn>
2023-12-12 13:07:04 +0700

    G

--- ASDict/MauiProgram.cs
+++ ASDict/MauiProgram.cs
@@ -16,6 +16,9 @@ namespace ASDict
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("PatrickHand-Regular.ttf", "PatrickHand");
+                    fonts.AddFont("Brands-Regular-400.otf", "FAB");
+                    fonts.AddFont("Free-Regular-400.otf", "FAR");
+                    fonts.AddFont("Free-Solid-900.otf", "FAS");
                 });
 
 #if DEBUG
