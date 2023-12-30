40fade78817b419f44d9f6961d419ef116029749
Tohew <baophong2401@gmail.com>
2023-12-30 10:09:26 +0700

    Q

--- ASDict/MVVM/Views/BookmarkScreenView.xaml.cs
+++ ASDict/MVVM/Views/BookmarkScreenView.xaml.cs
@@ -39,11 +39,14 @@ public partial class BookmarkScreenView : ContentPage
     private async void help_Clicked(object sender, EventArgs e)
     {
         if (isMenuOpen)
-            await DisplayAlert("help", "cc, cl", "OK");
+        {
+            string url = "https://nghgi.github.io/ASDict-Download/";
+            _ = Launcher.OpenAsync(new Uri(url));
+        }    
     }
     private async void infor_Clicked(object sender, EventArgs e)
     {
         if (isMenuOpen)
-            await DisplayAlert("info", "cc, cl", "OK");
+            await DisplayAlert("About us", "Welcome to our ASDict app! At GHQ, we're dedicated to crafting this tool to help you expand your vocabulary and use language more precisely. We offer an intuitive search for synonyms and antonyms, allowing you to gain a deeper understanding of word meanings and their contextual usage. With cross-platform support, our app is ready to accompany you across various devices. Thank you for choosing our app to enhance your language skills!", "OK");
     }
 }
